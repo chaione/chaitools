65709356eb165acd469d39a4c0dafd69ab81a6be
Travis Fischer <travis@chaione.com>
2016-11-03 13:38:07 -0500

    Initial Commit

---
+++ chaitools/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  chaitools
+//
+//  Created by Travis Fischer on 11/3/16.
+//  Copyright © 2016 ChaiOne. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
