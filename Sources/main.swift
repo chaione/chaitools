3c3c9bb16acb14662d111ab47caea42d63e7f8b8
Travis Fischer <travis@chaione.com>
2016-11-03 15:20:57 -0500

    Setup chaitools command line tool with the basic options.

---
+++ Sources/main.swift
@@ -0,0 +1,16 @@
+//
+//  main.swift
+//  chaitools
+//
+//  Created by Travis Fischer on 11/3/16.
+//  Copyright © 2016 ChaiOne. All rights reserved.
+//
+
+import Foundation
+import SwiftCLI
+
+CLI.setup(name: "chaitools", version: "1.0", description: "Brew some awesome apps with ChaiTools")
+
+
+
+CLI.go()
