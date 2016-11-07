4fbb5c000d29693f94e5174ae849d5c95b862fde
Alex Du Bois <alex.dubois@mac.com>
2016-11-07 10:26:02 -0600

    Clean up unused code

--- Sources/Template.swift
+++ Sources/Template.swift
@@ -9,29 +9,6 @@
 import Foundation
 import SwiftCLI
 
-enum Action: String {
-    case install = "install"
-    case update = "update"
-    case remove = "remove"
-
-    func generate() {
-        switch self {
-            case .install: print("Install the repo")
-            case .update: print("Update the repo")
-            case .remove: print("Remove the repo")
-        }
-    }
-
-    init?(_ string: String) {
-        switch string {
-            case "-i", "install": self = .install
-            case "-u", "update": self = .update
-            case "-r", "remove": self = .remove
-            default: return nil
-        }
-    }
-}
-
 class GitCommand {
     enum GitAction: String {
         case clone = "install"
@@ -55,7 +32,10 @@ class GitCommand {
     }
 
     func execute() {
-        guard let action = action, let remoteURL = remoteURL, let localURL = localURL else { return } // TODO: Do something more obvious
+        guard let action = action, let remoteURL = remoteURL, let localURL = localURL else {
+            NSLog("Git cannot operate without an action, remote URL, and local URL.")
+            return
+        }
 
         process.currentDirectoryPath = localURL.path
         process.arguments = action.arguments(with: remoteURL.absoluteString)
@@ -70,15 +50,6 @@ class Template: Command {
     var name: String = "template"
     var signature: String = "<action>"
     var shortDescription: String = "Install, update, or remove Xcode templates"
-
-    private var action: Action?
-    private var force: Bool = false
-
-    func setupOptions(options: OptionRegistry) {
-        options.add(flags: ["-f", "force"]) {
-            self.force = true
-        }
-    }
 
     func execute(arguments: CommandArguments) throws {
         let command = GitCommand()
