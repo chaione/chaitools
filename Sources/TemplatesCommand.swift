ac1913a9a2aa8b0587f0bcd1bd8e12fb3150e07d
Travis Fischer <travis@Mazikeen.local>
2017-03-17 15:10:36 -0500

    Modified templates command to install both file and project templates.

--- Sources/TemplatesCommand.swift
+++ Sources/TemplatesCommand.swift
@@ -15,19 +15,37 @@ enum TemplateActions : String {
     case remove
 }
 
+
+struct TemplatesSet {
+    var remoteURL: URL
+    var localDir: URL
+    var repo: GitRepo
+    
+    init(repoURL : URL, dir : URL) {
+        localDir = dir
+        remoteURL = repoURL
+        repo = GitRepo(withLocalURL: localDir, andRemoteURL: remoteURL)
+    }
+}
+
+
 @available(OSX 10.12, *)
 class TemplatesCommand: Command {
 
     var name: String = "templates"
     var signature: String = "<action>"
     var shortDescription: String = "Install, update, or remove Xcode templates"
+    
+    private var templates: [TemplatesSet] = []
 
-    private let templateRepoURL = URL(string: "git@bitbucket.org:chaione/chaitemplates.git")!
-    private let templateDirectory = FileManager.default.homeDirectoryForCurrentUser.appendingPathComponent("Library/Developer/Xcode/Templates/ChaiOne", isDirectory: true)
-    private let repo: GitRepo
     
     init() {
-        repo = GitRepo(withLocalURL: templateDirectory, andRemoteURL: templateRepoURL)
+        
+        templates.append(TemplatesSet(repoURL: URL(string: "git@bitbucket.org:chaione/chaitemplates.git")!,
+                                      dir: FileManager.default.homeDirectoryForCurrentUser.appendingPathComponent("Library/Developer/Xcode/Templates/File Templates/ChaiOne", isDirectory: true)))
+        templates.append(TemplatesSet(repoURL: URL(string: "git@bitbucket.org:chaione/chaixcodetemplates.git")!,
+                                      dir: FileManager.default.homeDirectoryForCurrentUser.appendingPathComponent("Library/Developer/Xcode/Templates/Project Templates/ChaiOne", isDirectory: true)))
+ 
     }
 
     func execute(arguments: CommandArguments) throws {
@@ -46,7 +64,10 @@ class TemplatesCommand: Command {
     
     private func installTemplates() {
         print("Attempting to install Xcode templates...")
-        let status = repo.execute(GitAction.clone)
+        var status = true
+        for template in templates {
+            status = status && template.repo.execute(GitAction.clone)
+        }
         if status {
             print("Successfully installed Xcode templates. 🎉")
         } else {
@@ -56,7 +77,10 @@ class TemplatesCommand: Command {
     
     private func updateTemplates() {
         print("Attempting to update Xcode templates...")
-        let status = repo.execute(GitAction.pull)
+        var status = true
+        for template in templates {
+            status = status && template.repo.execute(GitAction.pull)
+        }
         if status {
             print("Successfully updated Xcode templates. 🎉")
         } else {
@@ -68,16 +92,17 @@ class TemplatesCommand: Command {
         var isDirectory: ObjCBool = ObjCBool(true)
 
         print("Attempting to remove the templates directory...")
-
-        if FileManager.default.fileExists(atPath: templateDirectory.path, isDirectory: &isDirectory) {
-            do {
-                try FileManager.default.removeItem(atPath: templateDirectory.path)
-                print("Successfully removed the templates directory. 🎉")
-            } catch {
-                print("❗️ Error removing the directory. \(error)")
+        for template in templates {
+            if FileManager.default.fileExists(atPath: template.localDir.path, isDirectory: &isDirectory) {
+                do {
+                    try FileManager.default.removeItem(atPath: template.localDir.path)
+                    print("Successfully removed the templates directory. 🎉")
+                } catch {
+                    print("❗️ Error removing the directory. \(error)")
+                }
+            } else {
+                print("The templates directory does not exist, so it cannot be removed. 🤔")
             }
-        } else {
-            print("The templates directory does not exist, so it cannot be removed. 🤔")
         }
     }
 }
