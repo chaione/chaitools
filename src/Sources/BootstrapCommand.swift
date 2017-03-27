d1a82082040a3fe781b72d7f8f7be0ec45a92331
Travis Fischer <travis@Mazikeen.local>
2017-03-27 17:46:43 +0530

    Add default ReadMe.md file for each of the different subfolder structure.

--- src/Sources/BootstrapCommand.swift
+++ src/Sources/BootstrapCommand.swift
@@ -16,9 +16,13 @@ class BootstrapCommand: Command {
     var signature: String = "[<stack>]"
     var shortDescription: String = "Setup a ChaiOne starter project for the given tech stack"
 
+    private var projectName: String = ""
+
     func execute(arguments _: CommandArguments) throws {
         print("These boots are made for walking.")
-        setupDirectoryStructure()
+        if let projectURL = setupDirectoryStructure() {
+            setupReadMeDefaults(projectURL)
+        }
     }
 
     /// Setups the expected project folder structure:
@@ -27,8 +31,10 @@ class BootstrapCommand: Command {
     /// |-- scripts/
     /// |-- src/
     /// |-- tests/
-    func setupDirectoryStructure() {
-        let projectName = Input.awaitInput(message: "❓ What is the name of the project?")
+    func setupDirectoryStructure() -> URL? {
+
+        projectName = Input.awaitInput(message: "❓ What is the name of the project?")
+
         let projectDirURL = URL(fileURLWithPath: FileManager.default.currentDirectoryPath).appendingPathComponent(projectName)
 
         // Do not overwrite existing projects
@@ -50,6 +56,23 @@ class BootstrapCommand: Command {
             FileOps.defaultOps.createSubDirectory("scripts", parent: projectDirURL)
             FileOps.defaultOps.createSubDirectory("tests", parent: projectDirURL)
             FileOps.defaultOps.createSubDirectory("docs", parent: projectDirURL)
+
+            return projectDirURL
         }
+
+        return nil
+    }
+
+    /// Adds a dummy ReadMe.md file to each directory in the default system.
+    ///
+    /// - Parameter projectURL: File path URL for the main project directory.
+    func setupReadMeDefaults(_ projectURL: URL) {
+
+        FileManager.default.createFile(atPath: projectURL.appendingPathComponent("ReadMe.md").path, contents: "#Welcome to the \(projectName) project".data(using: .utf8))
+
+        FileManager.default.createFile(atPath: projectURL.appendingPathComponent("src/ReadMe.md").path, contents: "Project source code goes here.".data(using: .utf8))
+        FileManager.default.createFile(atPath: projectURL.appendingPathComponent("scripts/ReadMe.md").path, contents: "Project external scripts go here.".data(using: .utf8))
+        FileManager.default.createFile(atPath: projectURL.appendingPathComponent("tests/ReadMe.md").path, contents: "Automated tests goes here.".data(using: .utf8))
+        FileManager.default.createFile(atPath: projectURL.appendingPathComponent("docs/ReadMe.md").path, contents: "Project documentation goes here.".data(using: .utf8))
     }
 }
