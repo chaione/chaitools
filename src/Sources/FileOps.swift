chaione/chaitools

commit 5c5797caa94c2ce37c787b977881e35a689578b8

Generate default folder structure for a bootstrapped project.

src/Sources/FileOps.swift
@@ -26,12 +26,11 @@ class FileOps: NSObject {
     /// - Parameter dirURL: The local URL of the directory to be created
     /// - Returns: True if succeeds and false otherwise.
     func ensureDirectory(_ dirURL: URL) -> Bool {
-        var isDirectory: ObjCBool = ObjCBool(true)
-        if !FileManager.default.fileExists(atPath: dirURL.path, isDirectory: &isDirectory) {
+        if doesDirectoryExist(dirURL) {
             do {
                 print("The local directory does not exist. Attempting to create it...")
                 try FileManager.default.createDirectory(at: dirURL, withIntermediateDirectories: true)
-                print("Successfully created the directory.")
+                print("Successfully created the directory. 🎉")
                 return true
             } catch {
                 print("❗️ Error creating the directory. \(error)")
@@ -40,6 +39,14 @@ class FileOps: NSObject {
         }
 
         return true
+    }
+
+    /// Convenience method to check if a directory exists
+    ///
+    /// - Returns: True if the directory exists, false otherwise.
+    func doesDirectoryExist(_ url: URL) -> Bool {
+        var isDirectory: ObjCBool = ObjCBool(true)
+        return FileManager.default.fileExists(atPath: url.path, isDirectory: &isDirectory)
     }
 
     /// Generic actions to delete a given directory.
@@ -78,5 +85,20 @@ class FileOps: NSObject {
             // handle the error
         }
         return nil
+    }
+
+    /// Convenience method to create a subdirectory of a given directory
+    ///
+    /// - Parameters:
+    ///   - name: The name of the subdirectory to create
+    ///   - parent: The parent directory. Defaults to the current directory.
+    func createSubDirectory(_ name: String, parent: URL = URL(fileURLWithPath: FileManager.default.currentDirectoryPath)) {
+        // create substructure for project
+        do {
+            try FileManager.default.createDirectory(at: parent.appendingPathComponent(name), withIntermediateDirectories: true)
+            print("Successfully created \(name) subdirectory. 🎉")
+        } catch {
+            print("❗️ Failed to create \(name) subdirectory.")
+        }
     }
 }

src/Sources/BootstrapCommand.swift
@@ -9,6 +9,7 @@
 import Foundation
 import SwiftCLI
 
+@available(OSX 10.12, *)
 class BootstrapCommand: Command {
 
     var name: String = "bootstrap"
@@ -16,6 +17,39 @@ class BootstrapCommand: Command {
     var shortDescription: String = "Setup a ChaiOne starter project for the given tech stack"
 
     func execute(arguments _: CommandArguments) throws {
-        print("Those are some nice boots you got.")
+        print("These boots are made for walking.")
+        setupDirectoryStructure()
+    }
+
+    /// Setups the expected project folder structure:
+    /// |- <project_name>/
+    /// |-- docs/
+    /// |-- scripts/
+    /// |-- src/
+    /// |-- tests/
+    func setupDirectoryStructure() {
+        let projectName = Input.awaitInput(message: "❓ What is the name of the project?")
+        let projectDirURL = URL(fileURLWithPath: FileManager.default.currentDirectoryPath).appendingPathComponent(projectName)
+
+        // Do not overwrite existing projects
+        if FileOps.defaultOps.doesDirectoryExist(projectDirURL) {
+            print("❗️ Project \(projectName) already exists at this location.")
+        } else {
+
+            // create directory based on project name
+            print("Creating new project directory for project \(projectName)...")
+            let success = FileOps.defaultOps.ensureDirectory(projectDirURL)
+
+            if success {
+                print("Project directory created successfully.")
+            } else {
+                print("❗️ Project directory creation failed.")
+            }
+
+            FileOps.defaultOps.createSubDirectory("src", parent: projectDirURL)
+            FileOps.defaultOps.createSubDirectory("scripts", parent: projectDirURL)
+            FileOps.defaultOps.createSubDirectory("tests", parent: projectDirURL)
+            FileOps.defaultOps.createSubDirectory("docs", parent: projectDirURL)
+        }
     }
 }

src/chaitools.xcodeproj/project.pbxproj
@@ -409,6 +409,7 @@
 				FRAMEWORK_SEARCH_PATHS = "$(PLATFORM_DIR)/Developer/Library/Frameworks";
 				INFOPLIST_FILE = chaitools.xcodeproj/SwiftCLI_Info.plist;
 				LD_RUNPATH_SEARCH_PATHS = "$(TOOLCHAIN_DIR)/usr/lib/swift/macosx";
+				MACOSX_DEPLOYMENT_TARGET = 10.12;
 				OTHER_LDFLAGS = "$(inherited)";
 				OTHER_SWIFT_FLAGS = "$(inherited)";
 				PRODUCT_BUNDLE_IDENTIFIER = SwiftCLI;
@@ -451,6 +452,7 @@
 				FRAMEWORK_SEARCH_PATHS = "$(PLATFORM_DIR)/Developer/Library/Frameworks";
 				INFOPLIST_FILE = chaitools.xcodeproj/SwiftCLI_Info.plist;
 				LD_RUNPATH_SEARCH_PATHS = "$(TOOLCHAIN_DIR)/usr/lib/swift/macosx";
+				MACOSX_DEPLOYMENT_TARGET = 10.12;
 				OTHER_LDFLAGS = "$(inherited)";
 				OTHER_SWIFT_FLAGS = "$(inherited)";
 				PRODUCT_BUNDLE_IDENTIFIER = SwiftCLI;

commit 9b2b7c22935a78add0e65e75e6bd75e9ff6cfb5d

Fixes bug introduced by refactor in 5c5797c
If the directory existed, the system would constantly try to recreate it and fail because of the missing `!`.
Also made URL path generation more robust when dealing with directories.

src/Sources/FileOps.swift
@@ -26,7 +26,7 @@ class FileOps: NSObject {
     /// - Parameter dirURL: The local URL of the directory to be created
     /// - Returns: True if succeeds and false otherwise.
     func ensureDirectory(_ dirURL: URL) -> Bool {
-        if doesDirectoryExist(dirURL) {
+        if !doesDirectoryExist(dirURL) {
             do {
                 print("The local directory does not exist. Attempting to create it...")
                 try FileManager.default.createDirectory(at: dirURL, withIntermediateDirectories: true)
@@ -37,7 +37,6 @@ class FileOps: NSObject {
                 return false
             }
         }
-
         return true
     }
 
@@ -95,7 +94,7 @@ class FileOps: NSObject {
     func createSubDirectory(_ name: String, parent: URL = URL(fileURLWithPath: FileManager.default.currentDirectoryPath)) {
         // create substructure for project
         do {
-            try FileManager.default.createDirectory(at: parent.appendingPathComponent(name), withIntermediateDirectories: true)
+            try FileManager.default.createDirectory(at: parent.appendingPathComponent(name, isDirectory: true), withIntermediateDirectories: true)
             print("Successfully created \(name) subdirectory. 🎉")
         } catch {
             print("❗️ Failed to create \(name) subdirectory.")