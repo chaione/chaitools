635461e5aa530a66742084711546515ae85212df
Travis Fischer <travis.fischer@chaione.com>
2017-05-03 15:37:30 -0500

    Adds DevInit command structure

---
+++ src/Sources/ChaiToolsKit/DevInitCommand.swift
@@ -0,0 +1,41 @@
+//
+//  DevInitCommand.swift
+//  chaitools
+//
+//  Created by Travis Fischer on 5/3/17.
+//  Copyright © 2016 ChaiOne. All rights reserved.
+//
+
+import Foundation
+import SwiftCLI
+
+@available(OSX 10.12, *)
+public class DevInitCommand: OptionCommand {
+
+    public var name: String = "dev-init"
+    public var signature: String = ""
+    public var shortDescription: String = "Configure computer with default developer tech stacks"
+
+    public func setupOptions(options: OptionRegistry) {
+        MessageTools.addVerbosityOptions(options: options)
+    }
+
+    public init() {}
+
+    /// Executes the dev-init command
+    /// - Parameter arguments: The arguments passed to the command
+    public func execute(arguments: CommandArguments) throws {
+        MessageTools.state("Welcome to ChaiOne! Let's setup your machine.", color: .green, level: .silent)
+        // Install rbenv (using homebrew)
+        // Install fixed Ruby version (using rbenv, based on a configuration file)
+        // Install node (using homebrew)
+        // Install Ember-cli (using npm)
+        // Install react (using npm)
+        // Install react-native (using npm)
+        // Install tsrn (using npm locally)
+        // Install quicklook provisioning (direct download)
+        MessageTools.exclaim("All done! Go forth and make awesome stuff.", level: .silent)
+    }
+
+
+}
